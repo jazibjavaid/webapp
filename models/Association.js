ab12e9197e0c7f613053cba9369387557a46affc
jazibjavaid <javaid.j@northeastern.edu>
2023-11-18 00:56:11 -0500

    Added submission api and model

--- models/Association.js
+++ models/Association.js
@@ -1,5 +1,6 @@
 const Account = require('./models/account');
 const Assignment = require('./models/assignment');
+const Submission = require('./Submission.js');
 
 
 Account.hasMany(Assignment, {
@@ -10,6 +11,13 @@ Assignment.belongsTo(Account, {
     foreignKey: 'accountId'
 });
 
+Assignment.hasMany(Submission, {
+    foreignKey: 'assignment_id',
+    onDelete: 'CASCADE'
+});
 
+Submission.belongsTo(Assignment, { 
+    foreignKey: 'assignment_id' 
+});
 
-module.exports = { Account, Assignment };
+module.exports = { Account, Assignment, Submission };
